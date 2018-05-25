0bd72aa183b1970bae1eb383a16ac7b114b7e33c
Harrison <35613146+hmwirtz@users.noreply.github.com>
2018-05-24 19:43:44 -0700

    Actually works nowww

--- app/src/main/java/e/harrison/enginecalculator/Calculate.java
+++ app/src/main/java/e/harrison/enginecalculator/Calculate.java
@@ -1,29 +1,44 @@
 package e.harrison.enginecalculator;
 
 
+import android.content.Intent;
+
 public class Calculate {
 
-    private Variables variables;
 
-    public String dispCalc(){
+    public String dispCalc(Double Bore, Double Stroke, int Cylinders){
 
         double displacement, PI, radius;
 
         PI = 3.14159;
-        radius = variables.Bore /2;
-        displacement = radius * radius * variables.Stroke * PI * variables.Cy;
+        radius = Bore /2;
+        displacement = radius * radius * Stroke * PI * Cylinders;
 
         String disp = String.valueOf(displacement);
 
         return disp;
 
 
-
-
-
     }
 
+    public String boreStrokeRatio(Double Bore, Double Stroke){
 
+        Double ratio = Bore / Stroke;
+        String BSRatio = ratio.toString();
 
+        return BSRatio;
+    }
 
+    public String rodRatio(Double Stroke, Double rodLength){
+        Double ratio = rodLength / Stroke;
+        String rodRatio = ratio.toString();
+
+        return rodRatio;
+
+    }
 }
+
+
+
+
+
--- app/src/main/java/e/harrison/enginecalculator/MainActivity.java
+++ app/src/main/java/e/harrison/enginecalculator/MainActivity.java
@@ -1,5 +1,6 @@
 package e.harrison.enginecalculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,11 +10,11 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Calculate dispCalc;
-    private Button calculate;
-    private TextView dispOut;
-    private EditText Bore, Stroke, Cylinder;
-    private Variables variables;
+    TextView dispOut, BSRatio, rodRatio;
+    Button calculate;
+    EditText Bore, Stroke, Cylinder, RodLength;
+    Double bore, stroke, rodLength;
+    int cylinders;
 
 
     @Override
@@ -21,29 +22,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        calculate.setOnClickListener(calculateListener);
-        calculate = findViewById(R.id.calculate);
-        dispOut = findViewById(R.id.dispOut);
-        Bore = findViewById(R.id.BoreInput);
-        Stroke = findViewById(R.id.StrokeInput);
-        Cylinder = findViewById(R.id.CyInput);
-
-        variables.Bore = Double.parseDouble(Bore.getText().toString());
-        variables.Stroke = Double.parseDouble(Stroke.getText().toString());
-        variables.Cy = Integer.parseInt(Cylinder.getText().toString());
-
+        initializeViews();
 
 
     }
 
+    private void initializeViews (){
+
+
+
+        calculate = findViewById(R.id.calculate);
+        calculate.setOnClickListener(calculateListener);
+        dispOut = findViewById(R.id.dispOut);
+        Bore = findViewById(R.id.BoreInput);
+        Stroke = findViewById(R.id.StrokeInput);
+        Cylinder = findViewById(R.id.CyInput);
+        BSRatio = findViewById(R.id.boreStrokeRatio);
+        RodLength = findViewById(R.id.rodInput);
+        rodRatio = findViewById(R.id.rodRatioOut);
+
+
+    }
+
+
+    public void doAllCalcs(Double Bore, Double Stroke, Double RodLength, int Cylinders){
+
+        Calculate Calc = new Calculate();
+        dispOut.setText((Calc.dispCalc(Bore, Stroke, Cylinders)));
+        BSRatio.setText(Calc.boreStrokeRatio(Bore, Stroke));
+        rodRatio.setText(Calc.rodRatio(Stroke, RodLength));
+
+
+    }
 
 
     View.OnClickListener calculateListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
 
-            dispCalc
-
+            bore = Double.parseDouble(Bore.getText().toString());
+            stroke = Double.parseDouble(Stroke.getText().toString());
+            cylinders = Integer.parseInt(Cylinder.getText().toString());
+            rodLength = Double.parseDouble(RodLength.getText().toString());
+            doAllCalcs(bore, stroke,rodLength,cylinders);
 
 
         }
